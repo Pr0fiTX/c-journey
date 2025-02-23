7af5451a1f95941e779e74115380f3018a2168c9
Pr0fiTX <uventik999@gmail.com>
2025-02-23 20:08:56 +0300

    [ADD]: 12-1.c

---
+++ Books/C-Primer-Plus/Chapter-12/12-1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <sys/types.h>
+
+#define ANSW 56 // Expected input(answer)
+
+void critic(int n[restrict]);
+
+int main(void) {
+  int units = 0;
+
+  printf("=> How many kg does a barrel of butter weight?\n> ");
+  scanf("%d", &units);
+  while (units != ANSW) {
+    critic(&units);
+  }
+
+  return 0;
+}
+
+void critic(int n[restrict]) {
+  printf("=> Oh, ya really are an unlucky guy. Try again.\n> ");
+  scanf("%d", n);
+}
